ea282b0493c7a6246a75274c3349d4e770c9a202
gzn <gzndamon@gmail.com>
2017-05-06 12:45:26 +0800

    implement posts pagination with will_paginate

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,14 +3,14 @@ class PostsController < ApplicationController
 
   def new
     @group = Group.find(params[:group_id])
-    @post =Post.new
+    @post = Post.new
   end
 
   def create
     @group = Group.find(params[:group_id])
-    @post = Post.new[post_params]
+    @post = Post.new(post_params)
     @post.group = @group
-    @post.user = current.user
+    @post.user = current_user
     if @post.save
       redirect_to group_path(@group)
     else
@@ -18,11 +18,8 @@ class PostsController < ApplicationController
     end
   end
 
-  def rake
-  end
 
-  def routes
-  end
+private
 
   def post_params
     params.require(:post).permit(:content)
