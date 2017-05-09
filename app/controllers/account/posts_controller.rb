3c7a7cbb2a4ed887f29bf5828f5da05e616aff80
gzn <gzndamon@gmail.com>
2017-05-09 11:28:10 +0800

    add post delete/edit function

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -3,4 +3,36 @@ class Account::PostsController < ApplicationController
   def index
     @posts = current_user.posts
   end
+  def new
+    @group = Group.find(params[:group_id])
+    @post = Post.new
+  end
+
+  def create
+    @group = Group.find(params[:group_id])
+    @post = Post.new(post_params)
+    @post.group = @group
+    @post.user = current_user
+    if @post.save
+      redirect_to group_path(@group)
+    else
+      render :new
+    end
+
+  end
+
+
+private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
+  def check_if_memeber_of_group
+    @group = Group.find(params[:group_id])
+    if !current_user.is_member_of?(@group)
+      redirect_to group_path(@group), alert:"you have no permission"
+    end
+  end
+
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, :only => [:new, :create]
-
+  before_action :check_if_memeber_of_group, :only=> [:new, :create]
   def new
     @group = Group.find(params[:group_id])
     @post = Post.new
@@ -16,6 +16,34 @@ class PostsController < ApplicationController
     else
       render :new
     end
+
+    def destroy
+      @group = Group.find(params[:group_id])
+      @post = Post.find(params[:id])
+      @post.group = @group
+      @post.destroy
+      redirect_to account_posts_path, alert: "Post deleted"
+    end
+
+    def edit
+       @group = Group.find(params[:group_id])
+       @post = Post.find(params[:id])
+       @post.group = @group
+     end
+
+    def update
+       @group = Group.find(params[:group_id])
+       @post = Post.find(params[:id])
+       @post.group = @group
+       @post.user = current_user
+
+       if @post.update(post_params)
+         redirect_to account_posts_path, notice: "Update Success"
+       else
+         render :edit
+       end
+     end
+
   end
 
 
@@ -23,6 +51,13 @@ private
 
   def post_params
     params.require(:post).permit(:content)
+  end
+
+  def check_if_memeber_of_group
+    @group = Group.find(params[:group_id])
+    if !current_user.is_member_of?(@group)
+      redirect_to group_path(@group), alert:"you have no permission"
+    end
   end
 
 end
